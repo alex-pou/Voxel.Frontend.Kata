ea07b0a0aa3f9046094fb524a00f730a2b1a2d54
Alex Pou <apou@voxelgroup.net>
2022-12-01 18:21:51 +0100

    test: show images

--- src/app/tv-shows/components/grid/tv-shows-grid.component.spec.ts
+++ src/app/tv-shows/components/grid/tv-shows-grid.component.spec.ts
@@ -5,6 +5,37 @@ import { of } from 'rxjs';
 import { TvShowService } from '../../services/tv-show.service';
 import { TvShowsGridComponent } from './tv-shows-grid.component';
 
+const expectedResponse = [{
+  title: 'title1',
+  rating: 'rating1',
+  image: 'image1'
+},
+{
+  title: 'title2',
+  rating: 'rating2',
+  image: 'image2'
+},
+{
+  title: 'title3',
+  rating: 'rating3',
+  image: 'image3'
+},
+{
+  title: 'title4',
+  rating: 'rating4',
+  image: 'image4'
+},
+{
+  title: 'title5',
+  rating: 'rating5',
+  image: 'image5'
+},
+{
+  title: 'title6',
+  rating: 'rating6',
+  image: 'image6'
+}];
+
 describe('TvShowsGridComponent should', () => {
   let component: TvShowsGridComponent;
   let fixture: ComponentFixture<TvShowsGridComponent>;
@@ -16,7 +47,7 @@ describe('TvShowsGridComponent should', () => {
         HttpClientTestingModule
       ],
       declarations: [TvShowsGridComponent],
-      providers: [ TvShowService ]
+      providers: [TvShowService]
     })
       .compileComponents();
 
@@ -30,44 +61,25 @@ describe('TvShowsGridComponent should', () => {
     expect(component).toBeTruthy();
   });
 
-  fit('show six shows title', () => {
-    const expectedResponse = [{
-      title: 'title1',
-      rating: 'rating1',
-      image: 'image1'
-    },
-    {
-      title: 'title2',
-      rating: 'rating2',
-      image: 'image2'
-    },
-    {
-      title: 'title3',
-      rating: 'rating3',
-      image: 'image3'
-    },
-    {
-    title: 'title4',
-    rating: 'rating4',
-    image: 'image4'
-    },
-    {
-      title: 'title5',
-      rating: 'rating5',
-      image: 'image5'
-    },
-    {
-      title: 'title6',
-      rating: 'rating6',
-      image: 'image6'
-    }];
-
+  it('show six shows title', () => {
     spyOn(tvShowsService, 'getShows').and.returnValue(of(expectedResponse));
 
     component.ngOnInit();
     fixture.detectChanges();
 
     const showTitles = fixture.debugElement.queryAll(By.css('.show-title'));
+
+    expect(component.shows.length).toBe(6);
+    expect(showTitles.length).toBe(6);
+  })
+
+  fit('show six shows images', () => {
+    spyOn(tvShowsService, 'getShows').and.returnValue(of(expectedResponse));
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    const showTitles = fixture.debugElement.queryAll(By.css('.show-image'));
 
     expect(component.shows.length).toBe(6);
     expect(showTitles.length).toBe(6);
