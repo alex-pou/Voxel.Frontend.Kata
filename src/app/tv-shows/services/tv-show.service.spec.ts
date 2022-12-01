76d5c68619fd96336b3eada6280f51c90b1be098
Alex Pou <apou@voxelgroup.net>
2022-12-01 18:26:38 +0100

    FIX: fix tests

--- src/app/tv-shows/services/tv-show.service.spec.ts
+++ src/app/tv-shows/services/tv-show.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { TvShowService } from './tv-show.service';
@@ -6,7 +7,9 @@ describe('TvShowService', () => {
   let service: TvShowService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
     service = TestBed.inject(TvShowService);
   });
 
